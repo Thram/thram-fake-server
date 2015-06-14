5f9b9e22b14c59b1765d3051f89c5fc1678110ea
thram <thramposo@gmail.com>
2015-06-15 02:05:07 +1200

    Fix gitignore

--- scripts/app.js
+++ scripts/app.js
@@ -5,9 +5,16 @@ var App = (function () {
     function init() {
         $(".button-collapse").sideNav();
         $('.modal-trigger').leanModal();
+        $("#add-endpoint").on('click', function () {
+            var url = $('#app-name').val() + '/' + $('#endpoint').val();
+            $.post(url, {schema: $('#schema').val()}).success(function (res) {
+                alert(JSON.stringify(res));
+            });
+        });
 
         console.log('App Init :D')
     }
+
     return {init: init}
 
 })();
